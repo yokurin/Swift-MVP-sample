8f22b133e9738a115a5355cf185fb72bf7a6479c
Tsubasa Hayashi <yoku.rin.99@gmail.com>
2017-01-09 09:51:51 +0900

    Initial Commit

---
+++ SwiftMVPSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftMVPSample
+//
+//  Created by Hayashi Tsubasa on 2017/01/09.
+//  Copyright © 2017年 Tsubasa Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
